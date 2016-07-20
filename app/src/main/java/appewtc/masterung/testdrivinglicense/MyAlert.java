952539426dfe75fd09aecd560c57feb7727faa7d
masterUNG <phrombutr@gmail.com>
2016-07-20 14:23:43 +0700

    Sign Up Success

---
+++ app/src/main/java/appewtc/masterung/testdrivinglicense/MyAlert.java
@@ -0,0 +1,29 @@
+package appewtc.masterung.testdrivinglicense;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by masterUNG on 7/20/2016 AD.
+ */
+public class MyAlert {
+
+    public void myDialog(Context context,
+                         String strTitle,
+                         String strMessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.nobita48);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+}   // Main Class
--- app/src/main/java/appewtc/masterung/testdrivinglicense/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/testdrivinglicense/SignUpActivity.java
@@ -1,13 +1,130 @@
 package appewtc.masterung.testdrivinglicense;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, surnameEditText, ageEditText,
+            userEditText, passwordEditText;
+    private String nameString, surnameString, ageString,
+            userString, passwordString;
+    private String urlPHP = "http://swiftcodingthai.com/toey/add_user_master.php";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+        //Initial Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        surnameEditText = (EditText) findViewById(R.id.editText2);
+        ageEditText = (EditText) findViewById(R.id.editText3);
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
+
+
+    }   // Main Method
+
+    public void clickSignUpSign(View view) {
+
+        //Get Value from Edit Text
+        nameString = nameEditText.getText().toString().trim();
+        surnameString = surnameEditText.getText().toString().trim();
+        ageString = ageEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (nameString.equals("") || surnameString.equals("") ||
+                ageString.equals("") || userString.equals("") ||
+                passwordString.equals("")) {
+            //Have Space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกข้อมูล ทุกช่องคะ");
+
+        } else {
+            //No Space
+            confirmUser();
+
+        }   // if
+
+    }   // clickSign
+
+    private void confirmUser() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.nobita48);
+        builder.setTitle("โปรดตรวจข้อมูล");
+        builder.setMessage("Name = " + nameString + "\n" +
+                "Surname = " + surnameString + "\n" +
+                "Age = " + ageString + "\n" +
+                "User = " + userString + "\n" +
+                "Password = " + passwordString);
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                upLoadNewUser();
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+    }   // confirmUser
+
+    private void upLoadNewUser() {
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("Name", nameString)
+                .add("Surname", surnameString)
+                .add("Age", ageString)
+                .add("User", userString)
+                .add("Password", passwordString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(urlPHP).post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+
+                finish();
+
+            }
+        });
+
+
+    }   //upload
+
+
+}   // Main Class
