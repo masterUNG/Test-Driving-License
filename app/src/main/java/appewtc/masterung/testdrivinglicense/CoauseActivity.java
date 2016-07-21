ce5ec5ae33c177cd2d9295740256101d32b859d9
masterUNG <phrombutr@gmail.com>
2016-07-21 11:48:38 +0700

    Coaurse List Success

--- app/src/main/java/appewtc/masterung/testdrivinglicense/CoauseActivity.java
+++ app/src/main/java/appewtc/masterung/testdrivinglicense/CoauseActivity.java
@@ -1,16 +1,21 @@
 package appewtc.masterung.testdrivinglicense;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class CoauseActivity extends AppCompatActivity {
 
@@ -35,6 +40,7 @@ public class CoauseActivity extends AppCompatActivity {
         private Context context;
         private ListView coaurseListView;
         private static final String urlJSON = "http://swiftcodingthai.com/toey/get_coaurse_master.php";
+        private String[] titleStrings, detailStrings, imageStrings;
 
         public SynCoaurse(Context context,
                           ListView coaurseListView) {
@@ -64,6 +70,46 @@ public class CoauseActivity extends AppCompatActivity {
             super.onPostExecute(s);
 
             Log.d("21JulyV1", "JSON ==> " + s);
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(s);
+
+                titleStrings = new String[jsonArray.length()];
+                detailStrings = new String[jsonArray.length()];
+                imageStrings = new String[jsonArray.length()];
+
+                for (int i=0;i<jsonArray.length();i+=1) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    titleStrings[i] = jsonObject.getString("Title");
+                    detailStrings[i] = jsonObject.getString("Detail");
+                    imageStrings[i] = jsonObject.getString("Image");
+
+                    Log.d("21JulyV2", "image(" + i + ")= " + imageStrings[i].length());
+
+                }   // for
+
+                CoaurseAdapter coaurseAdapter = new CoaurseAdapter(context,
+                        titleStrings, detailStrings);
+                coaurseListView.setAdapter(coaurseAdapter);
+
+                coaurseListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                        Intent intent = new Intent(CoauseActivity.this, DetailCoaruseActivity.class);
+                        intent.putExtra("Title", titleStrings[i]);
+                        intent.putExtra("Detail", detailStrings[i]);
+                        intent.putExtra("Image", imageStrings[i]);
+                        startActivity(intent);
+
+                    }   // onItemClick
+                });
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
         }   // onPost
 
