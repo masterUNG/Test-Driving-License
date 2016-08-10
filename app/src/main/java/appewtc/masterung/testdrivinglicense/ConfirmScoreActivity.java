5070a136529986838ba32bb46421cdbfc1119fcd
masterUNG <phrombutr@gmail.com>
2016-08-10 13:11:02 +0700

    Get Current Time

--- app/src/main/java/appewtc/masterung/testdrivinglicense/ConfirmScoreActivity.java
+++ app/src/main/java/appewtc/masterung/testdrivinglicense/ConfirmScoreActivity.java
@@ -1,15 +1,18 @@
 package appewtc.masterung.testdrivinglicense;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class ConfirmScoreActivity extends AppCompatActivity {
 
     //Explicit
     private String[] loginStrings;
-    private String scoreString;
+    private String scoreString, dateString;
     private TextView nameTextView, dateTextView, scoreTextView;
 
     @Override
@@ -26,10 +29,16 @@ public class ConfirmScoreActivity extends AppCompatActivity {
         loginStrings = getIntent().getStringArrayExtra("login");
         scoreString = getIntent().getStringExtra("Score");
 
+        //Get Current Time
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+        dateString = dateFormat.format(calendar.getTime());
 
         //Show View
         nameTextView.setText(loginStrings[1] + " "  + loginStrings[2]);
         scoreTextView.setText("คะแนนของคุณ = " + scoreString);
+        dateTextView.setText(dateString);
 
 
     }   // Main Method
